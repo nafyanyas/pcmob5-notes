098e8ed542d5e9d1a45b71b454f96b122a99c501
adam.u <ubad.adam@gmail.com>
2022-08-06 16:58:26 +0800

    loginto pofile updated

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,78 +1,108 @@
-import { useNavigation } from "@react-navigation/native";
-import React, { useState } from "react";
 import {
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import { PROFILE_SCREEN } from "../constants";
-
-export default function LoginScreen() {
-  const navigation = useNavigation();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Login to your account</Text>
-
-      <TextInput
-        style={styles.inputView}
-        placeholder="Email"
-        value={username}
-        onChangeText={(username) => setUsername(username)}
-      />
-
-      <TextInput
-        style={styles.inputView}
-        placeholder="Password"
-        secureTextEntry={true}
-        value={password}
-        onChangeText={(pw) => setPassword(pw)}
-      />
-
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          navigation.navigate(PROFILE_SCREEN);
-        }}
-      >
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: 100,
-    padding: 25,
-  },
-  title: {
-    fontWeight: "bold",
-    fontSize: 40,
-    marginBottom: 50,
-  },
-  inputView: {
-    backgroundColor: "#F1F0F5",
-    borderRadius: 5,
-    marginBottom: 30,
-    padding: 20,
-  },
-  button: {
-    backgroundColor: "black",
-    borderRadius: 15,
-    width: "100%",
-  },
-  buttonText: {
-    textAlign: "center",
-    fontWeight: "400",
-    fontSize: 17,
-    padding: 20,
-    color: "white",
-  },
-});
+    Keyboard,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+  } from "react-native";
+  import React, { useState } from "react";
+  import { useNavigation } from "@react-navigation/native";
+  import axios from "axios";
+  import AsyncStorage from "@react-native-async-storage/async-storage";
+  import { PROFILE_SCREEN } from "../constants";
+  
+  // TODO: Change to your user name
+  const API = "https://pcmob5-blog-api-1.ubaidullaadam.repl.co";
+  const API_LOGIN = "/auth";
+  
+  export default function LoginScreen() {
+    const navigation = useNavigation();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorText, setErrorText] = useState("");
+  
+    async function login() {
+      Keyboard.dismiss();
+      try {
+        const response = await axios.post(API + API_LOGIN, {
+          username,
+          password,
+        });
+        await AsyncStorage.setItem("token", response.data.access_token);
+        navigation.navigate(PROFILE_SCREEN);
+      } catch (error) {
+        console.log(error.response);
+        setErrorText(error.response.data.description);
+      }
+    }
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Login to your account</Text>
+  
+        <TextInput
+          style={styles.inputView}
+          placeholder="Email"
+          value={username}
+          onChangeText={(username) => setUsername(username)}
+        />
+  
+        <TextInput
+          style={styles.inputView}
+          placeholder="Password"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(pw) => setPassword(pw)}
+        />
+  
+        <TouchableOpacity
+          style={styles.button}
+          onPress={async () => {
+            await login();
+          }}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+  
+        <Text style={styles.errorText}>{errorText}</Text>
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    errorText: {
+      marginTop: 20,
+      fontSize: 15,
+      color: "red",
+    },
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      paddingTop: 100,
+      padding: 25,
+    },
+    title: {
+      fontWeight: "bold",
+      fontSize: 40,
+      marginBottom: 50,
+    },
+    inputView: {
+      backgroundColor: "#F1F0F5",
+      borderRadius: 5,
+      marginBottom: 30,
+      padding: 20,
+    },
+    button: {
+      backgroundColor: "black",
+      borderRadius: 15,
+      width: "100%",
+    },
+    buttonText: {
+      textAlign: "center",
+      fontWeight: "400",
+      fontSize: 17,
+      padding: 20,
+      color: "white",
+    },
+  });
+  
